f4e7e0cb55b9965c6328a4a013cc5f3d1238fd5f
MickKav <mkavanagh187@gmail.com>
2023-10-17 22:02:58 +0000

    Change Allowed hots value

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mick-kavs-django-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [x.strip() for x in os.environ.get("ALLOWED_HOSTS", "").split(",")]
 
 
 # Application definition
